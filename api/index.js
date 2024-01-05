9d5ba4c054512bd2f4c2009f4e712aebf815d52d
Elias-Dewa <elias.dewa19@gmail.com>
2024-01-05 12:17:32 -0800

    Connect to database (mongoDB database)

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Connect to Mongoose
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.log(err))
 
 const app = express();
 
+
+// Listening a server
 app.listen(3000, () => {
   console.log("Server listening on port 3000...");
 });
