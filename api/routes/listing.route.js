a5fe1df434b599745040f76fca2bfd5d2129d426
Elias-Dewa <elias.dewa19@gmail.com>
2024-01-30 04:28:36 -0800

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
@@ -11,5 +11,7 @@ router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
 
 router.get('/get/:id', getListing);
+
+router.get('/get', getListings);
 
 export default router;
